b5c146bff666b1d19e9ea726a40be38ef1d2974b
Devdipoo <emiolaajileye@gmail.com git config --global user.name Devdipoogit config --global user.email emiolaajileye@gmail.com>
2024-04-26 12:18:46 +0100

    Input value function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const calculator = {
   operator: null,
 };
 
-function inputDigit(dot) {
+function inputDigit(digit) {
   const { displayValue, waitingForSecondOperand } = calculator;
 
   if (waitingForSecondOperand === true) {
